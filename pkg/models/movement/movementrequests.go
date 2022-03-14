b6eb29b426fcb53b78782d7d3b3e40cb1cabf44d
Lisa Kerle <l.kerle@trever.io>
2022-03-14 16:05:30 +0100

    [#27] Add function to get Movements for the last 24h

---
+++ pkg/models/movement/movementrequests.go
@@ -0,0 +1,37 @@
+package movement
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// CancelRequest represents an order cancel request.
+// An order can be cancelled using the internal ID or a
+// combination of Client ID (CID) and the daten for the given
+// CID.
+type MovementRequest struct {
+	Start int64 `json:"start,omitempty"`
+	End   int64 `json:"end,omitempty"`
+}
+
+func (mr *MovementRequest) ToJSON() ([]byte, error) {
+	resp := struct {
+		Start int64 `json:"start,omitempty"`
+		End   int64 `json:"end,omitempty"`
+	}{
+		Start: mr.Start,
+		End:   mr.End,
+	}
+
+	return json.Marshal(resp)
+}
+
+// MarshalJSON converts the order cancel object into the format required by the
+// bitfinex websocket service.
+func (mr *MovementRequest) MarshalJSON() ([]byte, error) {
+	b, err := mr.ToJSON()
+	if err != nil {
+		return nil, err
+	}
+	return []byte(fmt.Sprintf("[0, \"oc\", null, %s]", string(b))), nil
+}
--- v2/rest/movements.go
+++ v2/rest/movements.go
@@ -27,3 +27,26 @@ func (s *MovementService) Movements() (*movement.Snapshot, error) {
 
 	return os, nil
 }
+
+func (s *MovementService) MovementsIncludingTime(mr *movement.MovementRequest) (*movement.Snapshot, error) {
+	bytes, err := mr.ToJSON()
+	if err != nil {
+		return nil, err
+	}
+	req, err := s.requestFactory.NewAuthenticatedRequestWithBytes(common.PermissionRead, "movements/hist", bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := s.Request(req)
+	if err != nil {
+		return nil, err
+	}
+
+	os, err := movement.SnapshotFromRaw(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	return os, nil
+}
