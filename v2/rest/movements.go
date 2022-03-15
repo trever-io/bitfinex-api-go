fcf31fd8dd155021f37ba72ae454f46e8794338a
Lisa Kerle <l.kerle@trever.io>
2022-03-15 14:41:21 +0100

    small fix

--- v2/rest/movements.go
+++ v2/rest/movements.go
@@ -10,25 +10,7 @@ type MovementService struct {
 	Synchronous
 }
 
-func (s *MovementService) Movements() (*movement.Snapshot, error) {
-	req, err := s.requestFactory.NewAuthenticatedRequest(common.PermissionRead, "movements/hist")
-	if err != nil {
-		return nil, err
-	}
-	raw, err := s.Request(req)
-	if err != nil {
-		return nil, err
-	}
-
-	os, err := movement.SnapshotFromRaw(raw)
-	if err != nil {
-		return nil, err
-	}
-
-	return os, nil
-}
-
-func (s *MovementService) MovementsIncludingTime(mr *movement.MovementRequest) (*movement.Snapshot, error) {
+func (s *MovementService) Movements(mr *movement.MovementRequest) (*movement.Snapshot, error) {
 	bytes, err := mr.ToJSON()
 	if err != nil {
 		return nil, err
